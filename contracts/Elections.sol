9cc6294286486f8ca8857afa2e1293f2cd31245a
Mohit <mohitrakhade20@gmail.com>
2020-05-14 16:30:51 +0530

    e-vote completed

--- contracts/Elections.sol
+++ contracts/Elections.sol
@@ -8,6 +8,8 @@ contract Election {
         string name;
         uint256 voteCount;
     }
+    //store the account that we are voted
+    mapping(address => bool) public voters;
 
     //Store candidate
     //Fetch Candidate
@@ -15,6 +17,8 @@ contract Election {
 
     //store candidate count
     uint256 public candidatesCount;
+
+    event votedEvent(uint256 indexed _candidateId);
 
     constructor() public {
         addCandidate("Candidate 1");
@@ -24,5 +28,18 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        //  double vote problem
+        require(!voters[msg.sender]);
+        //  validate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        //  record that voter has voted
+        voters[msg.sender] = true;
+        //update candidate vote Count
+        candidates[_candidateId].voteCount++;
+        //trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
